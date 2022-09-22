2f05468442447de9ea830b089e7f805c66ae8462
Labyrinthegreg <leo.gregori95@gmail.com>
2022-09-22 15:10:34 +0200

    First commit

---
+++ starter-kit/src/main.js
@@ -0,0 +1,24 @@
+const express = require('express')
+require('dotenv').config({path: '.env'})
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+const logger = require('./utils/logger')
+
+const app = express()
+const port = process.env.HTTP_PORT
+
+// Middleware
+app.use(morgan('combined'))
+app.use(cors())
+app.use(bodyParser.json())
+
+// insert your router here
+
+
+//other
+
+app.listen(port, () => {
+    logger.log(`API listening at http://localhost:${port}`)
+})
+  
